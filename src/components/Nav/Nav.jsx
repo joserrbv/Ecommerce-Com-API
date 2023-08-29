c1c6154851c4c0d4fa8a6f66540fa2d0e7160a2e
joserrbv <118081152+joserrbv@users.noreply.github.com>
2023-08-28 21:38:40 -0300

    014 - Integração de categorias com API
    https://github.com/users/joserrbv/projects/1?pane=issue&itemId=36896974

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './index.css';
 import logo from "../../assets/e-commerce-logo.png";
 import lupa from "../../assets/lupa.svg"
 import { Link } from 'react-router-dom';
 
 export const Nav = () => {
+  const [categorias, setCategorias] = useState([]);
+  const [error, setError] = useState(null)
 
-  function submitPesquisa(event){
+  useEffect(() => {
+
+    (async () => {
+
+      try {
+        const resApi = await fetch('https://fakestoreapi.com/products/categories')
+        const resApiJson = await resApi.json()
+
+        if (resApi.ok) {
+          setCategorias(resApiJson)
+        } else {
+          throw resApi
+        }
+      } catch (error) {
+
+        setError({
+          titulo: 'Erro',
+          msg: 'Ocorreceu um erro inesperado, tente novamente mais tarde!'
+        })
+
+        console.error('erro no fetch', error);
+      }
+
+
+
+
+
+
+    })()
+
+  }, [])
+
+
+
+  function submitPesquisa(event) {
     event.preventDefault();
     // INICIO - obter valores do formulario
     let dadosFormJson = {};
@@ -19,29 +55,53 @@ export const Nav = () => {
     console.log(dadosFormJson);
   }
 
+
+
   return (
     <div className='nav'>
-        <div className="nav-top">
-          <Link to="/">
-            <img src={logo} alt="logo" className="logo" />
-          </Link>
+      <div className="nav-top">
 
-          <form onSubmit={()=>{submitPesquisa}} className='input-pesquisa'>
-            <input id='pesquisa' name="pesquisa" type="text" placeholder="O que você está procurando?" />
-            <button type="submit"><i className="font-white fa-solid fa-magnifying-glass fa-2x"></i></button> 
-          </form>
+        <Link to="/">
+          <img src={logo} alt="logo" className="logo" />
+        </Link>
 
-          <i className="font-white fa-solid fa-cart-shopping fa-2x"></i>
-        </div>
-        
-        <ul className='nav-bottom'>
-          <li>Novidades</li>
-          <li>Jogos</li>
-          <li>Video Games</li>
-          <li>Mesas Gamer</li>
-          <li>Promoções</li>
-          <li>Atendimento</li>
-        </ul>
+        <form onSubmit={() => { submitPesquisa }} className='input-pesquisa'>
+          <input id='pesquisa' name="pesquisa" type="text" placeholder="O que você está procurando?" />
+          <button type="submit"><i className="font-white fa-solid fa-magnifying-glass fa-2x"></i></button>
+        </form>
+
+        <i className="font-white fa-solid fa-cart-shopping fa-2x"></i>
+      </div>
+
+
+      <ul className='nav-bottom'>
+
+        {
+
+          (error !== null) ? (<>
+
+            <center>
+              <p><b>{error.titulo}:</b> {error.msg}</p>
+            </center>
+
+          </>) : (<>
+
+
+
+            {
+              categorias.map((categoria) => (<>
+                <Link to={`/categoria/${categoria}`}>
+                  <li>{categoria}</li>
+                </Link>
+              </>))
+            }
+
+          </>)
+
+        }
+
+
+      </ul>
     </div>
   )
 }
